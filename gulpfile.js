2eaf2836a59947803a26a8ffd69bc47632912d01
Robert Schönthal <caziel@gmx.net>
2015-03-08 22:19:18 +0100

    parallax as umd, iscroll for ios

--- gulpfile.js
+++ gulpfile.js
@@ -67,20 +67,20 @@ var scripts = function(minify) {
       map: false
     });
     return b.bundle()
-      .pipe(source(opts.mainJsInput))
-      .pipe(rename("app.js"))
-      .pipe(gulp.dest(opts.buildFolder));
+    .pipe(source(opts.mainJsInput))
+    .pipe(rename("app.js"))
+    .pipe(gulp.dest(opts.buildFolder));
   }
 
   return b.bundle()
-    .pipe(source(opts.mainJsInput))
-    .pipe(rename("app.js"))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({
-      loadMaps: true
-    }))
-    .pipe(sourcemaps.write(opts.buildFolder))
-    .pipe(gulp.dest(opts.buildFolder));
+  .pipe(source(opts.mainJsInput))
+  .pipe(rename("app.js"))
+  .pipe(buffer())
+  .pipe(sourcemaps.init({
+    loadMaps: true
+  }))
+  .pipe(sourcemaps.write(opts.buildFolder))
+  .pipe(gulp.dest(opts.buildFolder));
 }
 
 gulp.task('clean', function(cb) {
@@ -89,30 +89,30 @@ gulp.task('clean', function(cb) {
 
 gulp.task('svg', function() {
   var svgs = gulp
-    .src(opts.svgSources)
-    .pipe(cheerio({
-      run: function($) {
-        $('[fill]').removeAttr('fill');
-      },
-      parserOptions: {
-        xmlMode: true
-      }
-    }))
-    .pipe(rename(function(path) {
-      path.prefix = "icon-";
-      path.basename = path.basename.replace('-plain-wordmark', '');
-      path.basename = path.basename.replace('-original-wordmark', '');
-      path.basename = path.basename.replace('-plain', '');
-      path.basename = path.basename.replace('-original', '');
+  .src(opts.svgSources)
+  .pipe(cheerio({
+    run: function($) {
+      $('[fill]').removeAttr('fill');
+    },
+    parserOptions: {
+      xmlMode: true
     }
-    ))
-    .pipe(svgmin())
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(cheerio(function($) {
-      $('svg').attr('style', 'display:none');
-    }))
+  }))
+  .pipe(rename(function(path) {
+    path.prefix = "icon-";
+    path.basename = path.basename.replace('-plain-wordmark', '');
+    path.basename = path.basename.replace('-original-wordmark', '');
+    path.basename = path.basename.replace('-plain', '');
+    path.basename = path.basename.replace('-original', '');
+  }
+  ))
+  .pipe(svgmin())
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(cheerio(function($) {
+    $('svg').attr('style', 'display:none');
+  }))
   ;
 
   function fileContents(filePath, file) {
@@ -120,45 +120,45 @@ gulp.task('svg', function() {
   }
 
   return gulp
-    .src('./_index.html')
-    .pipe(minifyHTML({
-      comments: true
-    }))
-    .pipe(inject(svgs, {
-      transform: fileContents
-    }))
-    .pipe(rename("index.html"))
-    .pipe(gulp.dest('.'));
+  .src('./_index.html')
+  .pipe(minifyHTML({
+    comments: true
+  }))
+  .pipe(inject(svgs, {
+    transform: fileContents
+  }))
+  .pipe(rename("index.html"))
+  .pipe(gulp.dest('.'));
 });
 
 gulp.task('styles', ['sprite'], function() {
   var stream = gulp.src(opts.mainStylesInput)
-    //.pipe(gulpif(debug, sourcemaps.init()))
-    .pipe(sass({
-      includePaths: ["./node_modules/"]
-    }))
-    //.pipe(gulpif(debug, sourcemaps.write()))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulpif(!debug, minifyCss()))
-    .pipe(gulp.dest(opts.buildFolder))
-    .pipe(reload({
-      stream: true
-    }))
+  //.pipe(gulpif(debug, sourcemaps.init()))
+  .pipe(sass({
+    includePaths: ["./node_modules/"]
+  }))
+  //.pipe(gulpif(debug, sourcemaps.write()))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(gulpif(!debug, minifyCss()))
+  .pipe(gulp.dest(opts.buildFolder))
+  .pipe(reload({
+    stream: true
+  }))
 
 });
 
 gulp.task('images', function() {
   return gulp.src('./images/*.jpg')
-    .pipe(imagemin({}))
-    .pipe(gulp.dest(opts.buildFolder))
+  .pipe(imagemin({}))
+  .pipe(gulp.dest(opts.buildFolder))
 });
 
 gulp.task('fonts', function() {
   return gulp.src('./node_modules/materialize-css/font/roboto/*')
-    .pipe(gulp.dest(opts.buildFolder))
+  .pipe(gulp.dest(opts.buildFolder))
 });
 
 gulp.task('js', function() {
@@ -175,26 +175,26 @@ gulp.task('js', function() {
   }
 
   return b.bundle()
-    .pipe(source(opts.mainJsInput))
-    .pipe(rename("app.js"))
-    .pipe(buffer())
-    .pipe(gulpif(debug, sourcemaps.init({
-      loadMaps: true
-    })))
-    .pipe(gulpif(debug, sourcemaps.write(opts.buildFolder)))
-    .pipe(gulp.dest(opts.buildFolder));
+  .pipe(source(opts.mainJsInput))
+  .pipe(rename("app.js"))
+  .pipe(buffer())
+  .pipe(gulpif(debug, sourcemaps.init({
+    loadMaps: true
+  })))
+  .pipe(gulpif(debug, sourcemaps.write(opts.buildFolder)))
+  .pipe(gulp.dest(opts.buildFolder));
 
 });
 
 gulp.task('sprite', function() {
   return gulp.src(['./images/references/*', './images/me.png'])
-    .pipe(sprite({
-      base64: true,
-      style: '_base64.scss',
-      processor: 'scss',
-      prefix: 'reference'
-    }))
-    .pipe(gulp.dest('./styles'));
+  .pipe(sprite({
+    base64: true,
+    style: '_base64.scss',
+    processor: 'scss',
+    prefix: 'reference'
+  }))
+  .pipe(gulp.dest('./styles'));
 });
 
 gulp.task('browser-sync', function() {
--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 //var React = require('react');
+var IScroll = require('IScroll');
+var parallax = require('./parallax.js');
 
 function ready(fn) {
   if (document.readyState != 'loading') {
@@ -8,93 +10,7 @@ function ready(fn) {
   }
 }
 
-function getWindowSize() {
-  var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0];
-
-  return {
-    width: w.innerWidth || e.clientWidth || g.clientWidth,
-    height: w.innerHeight || e.clientHeight || g.clientHeight
-  };
-}
-
-function offset(elt) {
-  var rect = elt.getBoundingClientRect(),
-    bodyElt = document.body;
-
-  return {
-    top: rect.top + bodyElt .scrollTop,
-    left: rect.left + bodyElt .scrollLeft
-  };
-}
-
-function getHeight(elem) {
-  return Math.max(elem.offsetHeight, elem.clientHeight);
-}
-
-// vanilla js port of materialize/parallax
-function parallax(query) {
-  var window_width = getWindowSize.width;
-
-  Array.prototype.forEach.call(document.querySelectorAll(query), function(item, i) {
-    item.classList.add('parallax');
-
-    Array.prototype.forEach.call(item.querySelectorAll('img'), function(img, i) {
-      img.style.backgroundImage = 'url(' + img.src + ')';
-      img.src = 'data:image/gif;base64,R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
-    });
-
-    function updateParallax(initial) {
-      var container_height;
-      var image = item.querySelector('img');
-      if (window_width < 992) {
-        container_height = (getHeight(item) > 0) ? getHeight(item) : getHeight(image);
-      } else {
-        container_height = (getHeight(item) > 0) ? getHeight(item) : 500;
-      }
-      var img_height = getHeight(image);
-      var parallax_dist = img_height - container_height;
-      var bottom = offset(item).top + container_height;
-      var top = offset(item).top;
-      var scrollTop = window.pageYOffset;
-      var windowHeight = window.innerHeight;
-      var windowBottom = scrollTop + windowHeight;
-      var percentScrolled = (windowBottom - top) / (container_height + windowHeight);
-      var parallax = -1 * parallax_dist * percentScrolled;
-
-      if ((bottom > scrollTop) && (top < (scrollTop + windowHeight))) {
-        image.style.bottom = parallax + "px";
-      }
-      if (initial) {
-        image.style.display = "block";
-      }
-    }
-
-    // Wait for image load
-    Array.prototype.forEach.call(item.querySelectorAll('img'), function(img, i) {
-      img.addEventListener('load', function() {
-        updateParallax(true);
-      });
-      if (img.complete) {
-        img.dispatchEvent(new Event('load'));
-      }
-    });
-
-    window.addEventListener('scroll', function() {
-      window_width = getWindowSize.width;
-      updateParallax(false);
-    });
-
-    window.addEventListener('resize', function() {
-      window_width = getWindowSize.width;
-      updateParallax(false);
-    });
-  });
-}
-
 ready(function() {
-  parallax('.parallax');
+  new IScroll(document.body);
+  parallax.init(document.querySelectorAll('.parallax'));
 });
-
